7e7007750945d5e5dffdc0f25bfde29ac4fe98cc
KMSkelton <kmskelton@outlook.com>
2018-02-11 00:01:05 -0800

    updated render request

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,4 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/templates/blog/post_list.html', {'posts': posts})
